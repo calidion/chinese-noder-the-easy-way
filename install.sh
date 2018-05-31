e0a1f01861b7a627e70be7f279a7a044268de047
calidion <calidion@gmail.com>
2018-05-31 15:06:10 +0800

    updated

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 # 
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
 export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node  
 echo 'export NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node' >> ~/.profile   
 
-nvm install 6
-nvm install 7
-nvm alias default 6
+nvm install 8
+nvm install 10
+nvm alias default 8
 touch ~/.npmrc
 echo 'registry=http://r.cnpmjs.org/' >> ~/.npmrc
 nvm use 6
